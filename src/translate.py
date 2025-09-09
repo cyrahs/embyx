4c1763b3498f95438f2151a9ea7db26411f9f6e1
cyrahs <105589264+cyrahs@users.noreply.github.com>
2025-09-09 02:13:27 -0700

    Logic optimize for NFO translate

--- src/translate.py
+++ src/translate.py
@@ -6,7 +6,7 @@ from defusedxml.ElementTree import parse as xmlparse
 from tqdm import tqdm
 
 from src.core import config, logger
-from src.utils import get_avid, translator
+from src.utils import translator
 
 XML_ESCAPE = str.maketrans(
     {
@@ -15,7 +15,6 @@ XML_ESCAPE = str.maketrans(
         '>': '＞',  # noqa: RUF001
         '"': '＂',  # noqa: RUF001
         "'": '＇',  # noqa: RUF001
-        '\n': '<br>',
     },
 )
 
@@ -23,23 +22,10 @@ XML_ESCAPE = str.maketrans(
 log = logger.get('translate')
 
 
-def check_translated_by_title(title: str, original_title: str) -> bool:
-    if original_title in title:
-        return False
-    return re.sub(r'-cd\d+', '', title) != get_avid(title)
-
-
 def replace_xml(xml: str, tag: str, content: str) -> str:
-    # keep CDATA if present
-    def repl(m: re.Match) -> str:
-        inner = m.group(1)
-        if inner.startswith('<![CDATA[') and inner.endswith(']]>'):
-            return f'<{tag}><![CDATA[{content}]]></{tag}>'
-        return f'<{tag}>{content}</{tag}>'
-
     return re.sub(
         rf'<{tag}>(.*?)</{tag}>',
-        repl,
+        f'<{tag}>{content}</{tag}>',
         xml,
         flags=re.DOTALL,
         count=1,
@@ -47,35 +33,35 @@ def replace_xml(xml: str, tag: str, content: str) -> str:
 
 
 async def translate(xml_text: str) -> str:
-    if result := re.match(r'<!\[CDATA\[(.*?)\]\]>', xml_text):
-        inner_text = result.group(1)
-        translated_text = await translator.translate(inner_text.replace('<br>', '\n'))
-        return f'<![CDATA[{translated_text}]]>'
     result = await translator.translate(xml_text.replace('<br>', '\n'))
     return result.translate(XML_ESCAPE)
 
 
 async def process_title(title: str, original_title: str, xml: str) -> str:
     translated_ori_title = await translate(original_title)
+    translated_ori_title = translated_ori_title.replace('\n', ' ')
     if original_title in title:  # noqa: SIM108
         new_title = title.replace(original_title, translated_ori_title)
     else:
         new_title = title + ' ' + translated_ori_title
     xml = replace_xml(xml, 'title', new_title)
-    return replace_xml(xml, 'sorttitle', new_title)
+    xml = replace_xml(xml, 'sorttitle', new_title)
+    return insert_tag_below(xml, 'title', 'titletranslated', 'true')
 
 
 async def process_plot(plot: str, xml: str) -> str:
-    new_xml = copy_tag(xml, 'plot', 'originalplot')
+    new_xml = clone_tag(xml, 'plot', 'originalplot')
     # skip this if failed to copy tag
     if new_xml is None:
         log.error('Plot translation skipped because failed to copy tag')
         return xml
     translated_plot = await translate(plot)
+    # cover with CDATA
+    translated_plot = f'<![CDATA[{translated_plot.replace("\n", "<br>")}]]>'
     return replace_xml(new_xml, 'plot', translated_plot)
 
 
-def copy_tag(xml: str, tag: str, new_tag: str) -> str | None:
+def clone_tag(xml: str, tag: str, new_tag: str) -> str | None:
     pattern = rf'^(\s*)<{tag}>(.*?)</{tag}>'
 
     # Check if the tag exists first
@@ -91,6 +77,17 @@ def copy_tag(xml: str, tag: str, new_tag: str) -> str | None:
     return re.sub(pattern, repl, xml, flags=re.DOTALL | re.MULTILINE, count=1)
 
 
+def insert_tag_below(xml: str, tag: str, new_tag: str, content: str) -> str:
+    pattern = rf'^(\s*)<{tag}>(.*?)</{tag}>'
+
+    def repl(match: re.Match) -> str:
+        indent = match.group(1)
+        ori_content = match.group(2)
+        return f'{indent}<{tag}>{ori_content}</{tag}>\n{indent}<{new_tag}>{content}</{new_tag}>'
+
+    return re.sub(pattern, repl, xml, flags=re.DOTALL | re.MULTILINE, count=1)
+
+
 def get_process_list() -> dict[Path, dict[str, str]]:
     process_list = {}
     nfo_dir = config.translate.nfo_dir
@@ -101,10 +98,11 @@ def get_process_list() -> dict[Path, dict[str, str]]:
         original_title_elem = root.find('originaltitle')
         plot_elem = root.find('plot')
         original_plot_elem = root.find('originalplot')
+        title_translated_elem = root.find('titletranslated')
         if title_elem is None:
             log.error('Title not found in %s', nfo_path)
         # check if title need to be translated
-        if original_title_elem is not None and not check_translated_by_title(title_elem.text, original_title_elem.text):
+        if title_translated_elem is None and original_title_elem is not None:
             process_list[nfo_path] = {
                 'title': title_elem.text,
                 'original_title': original_title_elem.text,
@@ -129,8 +127,12 @@ async def process_one(nfo_path: Path, data: dict[str, str]) -> None:
 
 
 async def batch_process(batch_size: int = 10) -> None:
+    log.info('Start NFO translation')
     process_list = get_process_list()
-    log.info('%d items to translate', len(process_list))
+    if not process_list:
+        log.info('No items to translate')
+        return
+    log.info('Found %d items to translate', len(process_list))
     pbar = tqdm(total=len(process_list), desc='Processing')
 
     semaphore = asyncio.Semaphore(batch_size)
